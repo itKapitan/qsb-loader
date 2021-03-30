84d3f6b56bb027ec5221d2329ac05bbc719befb8
RadCor <radvancor@gmail.com>
2021-03-30 15:22:46 +0300

    добавление анимации

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function preloader() {
+    $(() => {
+        setInterval(() => {
+            let p = $(".preloader");
+            p.css("opacity", 0);
+            setInterval(() => p.remove(), parseInt(p.css("--duration")) * 1000);
+        }, 5000);
+    });
+}
+preloader();
